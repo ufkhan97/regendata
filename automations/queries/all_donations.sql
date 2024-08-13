2b3cf4d364fc5d2297d3609ec47357d4fcf57f52
Umar Khan <ufkhan97@gmail.com>
2024-08-13 16:03:55 -0400

    update grants program rounds mapping data source

--- automations/queries/all_donations.sql
+++ automations/queries/all_donations.sql
@@ -7,7 +7,7 @@ WITH gg_rounds AS (
       gg."round_name" AS "round_name",
       gg."round_id" AS "round_id"
     FROM
-      "GrantsProgramRoundsOnGrantsStack" gg),
+       "experimental_views"."all_rounds_20240810172652" gg),
 grants_stack_grants as (
     SELECT 
         gg."round_num",
