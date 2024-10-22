bc94a4786c3823bb8c6c7a644e32a7ecdff1af75
Umar Khan <ufkhan97@gmail.com>
2024-10-22 13:24:46 -0400

    Update update_indexer_schema.py

--- automations/update_indexer_schema.py
+++ automations/update_indexer_schema.py
@@ -19,31 +19,60 @@ except ImportError:
 
 
 # Constants for table names
-TABLES_TO_DROP = ['rounds', 'donations', 'applications_payouts', 'applications']#, 'round_roles']
-TABLES_TO_IMPORT = [ 'rounds', 'donations', 'applications_payouts']#, 'round_roles']
-APPLICATIONS_TABLE_DEFINITION = """
-CREATE FOREIGN TABLE indexer.applications (
-  id text OPTIONS (column_name 'id') COLLATE pg_catalog."default" NOT NULL,
-  chain_id integer OPTIONS (column_name 'chain_id') NOT NULL,
-  round_id text OPTIONS (column_name 'round_id') COLLATE pg_catalog."default" NOT NULL,
-  project_id text OPTIONS (column_name 'project_id') COLLATE pg_catalog."default",
-  anchor_address text OPTIONS (column_name 'anchor_address') COLLATE pg_catalog."default",
-  status text OPTIONS (column_name 'status'),  -- Simplified to text for testing
-  status_snapshots jsonb OPTIONS (column_name 'status_snapshots'),
-  distribution_transaction text OPTIONS (column_name 'distribution_transaction') COLLATE pg_catalog."default",
-  metadata_cid text OPTIONS (column_name 'metadata_cid') COLLATE pg_catalog."default",
-  metadata jsonb OPTIONS (column_name 'metadata'),
-  created_by_address text OPTIONS (column_name 'created_by_address') COLLATE pg_catalog."default",
-  created_at_block numeric(78,0) OPTIONS (column_name 'created_at_block'),
-  status_updated_at_block numeric(78,0) OPTIONS (column_name 'status_updated_at_block'),
-  total_donations_count integer OPTIONS (column_name 'total_donations_count'),
-  total_amount_donated_in_usd real OPTIONS (column_name 'total_amount_donated_in_usd'),
-  unique_donors_count integer OPTIONS (column_name 'unique_donors_count'),
-  tags text[] OPTIONS (column_name 'tags') COLLATE pg_catalog."default"
-)
-SERVER indexer
-OPTIONS (schema_name '{schema}', table_name 'applications');
-"""
+TABLES_TO_DROP = [
+    'round_roles',
+    'applications',
+    'rounds',
+    'donations',
+    'applications_payouts'
+]
+
+TABLES_TO_IMPORT = [
+    'rounds',
+    'donations',
+    'applications_payouts'
+]
+
+TABLES_TO_CREATE_FROM_DEFINITION = [
+    'round_roles',
+    'applications'
+]
+TABLE_DEFINITIONS = {
+    'applications': """
+    CREATE FOREIGN TABLE indexer.applications (
+      id text OPTIONS (column_name 'id') NOT NULL,
+      chain_id integer OPTIONS (column_name 'chain_id') NOT NULL,
+      round_id text OPTIONS (column_name 'round_id') NOT NULL,
+      project_id text OPTIONS (column_name 'project_id'),
+      anchor_address text OPTIONS (column_name 'anchor_address'),
+      status text OPTIONS (column_name 'status'),
+      status_snapshots jsonb OPTIONS (column_name 'status_snapshots'),
+      distribution_transaction text OPTIONS (column_name 'distribution_transaction'),
+      metadata_cid text OPTIONS (column_name 'metadata_cid'),
+      metadata jsonb OPTIONS (column_name 'metadata'),
+      created_by_address text OPTIONS (column_name 'created_by_address'),
+      created_at_block numeric(78,0) OPTIONS (column_name 'created_at_block'),
+      status_updated_at_block numeric(78,0) OPTIONS (column_name 'status_updated_at_block'),
+      total_donations_count integer OPTIONS (column_name 'total_donations_count'),
+      total_amount_donated_in_usd real OPTIONS (column_name 'total_amount_donated_in_usd'),
+      unique_donors_count integer OPTIONS (column_name 'unique_donors_count'),
+      tags text[] OPTIONS (column_name 'tags')
+    )
+    SERVER indexer
+    OPTIONS (schema_name '{schema}', table_name 'applications');
+    """,
+    'round_roles': """
+    CREATE FOREIGN TABLE indexer.round_roles (
+      chain_id integer OPTIONS (column_name 'chain_id') NOT NULL,
+      round_id text OPTIONS (column_name 'round_id') NOT NULL,
+      address text OPTIONS (column_name 'address') NOT NULL,
+      role text OPTIONS (column_name 'role') NOT NULL,
+      created_at_block numeric(78,0) OPTIONS (column_name 'created_at_block')
+    )
+    SERVER indexer
+    OPTIONS (schema_name '{schema}', table_name 'round_roles');
+    """
+}
 
 # Database connection details
 INDEXER_DB_PARAMS = {
@@ -62,17 +91,19 @@ DB_PARAMS = {
     'password': os.getenv('DB_PASSWORD')
 }
 
-
 def drop_foreign_tables(tables, db_params):
     """Drop specified foreign tables."""
     for table in tables:
         drop_command = f'DROP FOREIGN TABLE IF EXISTS indexer.{table} CASCADE;'
         db.execute_command(drop_command, db_params, logger)
 
-
-def create_applications_table(schema, db_params, logger=None):
-    """Create the applications table."""
-    create_command = APPLICATIONS_TABLE_DEFINITION.format(schema=schema)
+def create_table_from_definition(table_name, schema, db_params, logger=None):
+    """Create a table from its definition."""
+    logger.info(f"Creating table {table_name} in schema {schema}")
+    table_definition = TABLE_DEFINITIONS.get(table_name)
+    if table_definition is None:
+        raise ValueError(f"No definition found for table {table_name}")
+    create_command = table_definition.format(schema=schema)
     db.execute_command(create_command, db_params, logger)
 
 
@@ -88,7 +119,6 @@ def check_table_exists(schema, table, db_params):
     logger.info(f"Table {table} exists in {schema}: {result}")
 
 
-
 # Main execution logic
 def main():
     version_query = '''
@@ -101,19 +131,19 @@ def main():
     try:
         server = 'indexer'
         target_schema = 'indexer'
+
         # Uncomment the following lines to automatically fetch the latest schema version
         # version_result = db.run_query(version_query, INDEXER_DB_PARAMS, logger)
         # latest_schema_version = version_result['latest_schema_version'][0]
 
         # Manual override for schema version
-        # Change this value to update the schema version
-        latest_schema_version = 86
+        latest_schema_version = 86 # Change this value to update the schema version
         schema_name = f'chain_data_{latest_schema_version}'
-        check_table_exists(schema_name, 'round_roles', INDEXER_DB_PARAMS)
 
         drop_foreign_tables(TABLES_TO_DROP, DB_PARAMS)
         db.import_foreign_schema(server, schema_name, TABLES_TO_IMPORT, DB_PARAMS, target_schema, logger=logger)
-        create_applications_table(schema_name, DB_PARAMS, logger)
+        for table in TABLES_TO_CREATE_FROM_DEFINITION:
+            create_table_from_definition(table, schema_name, DB_PARAMS, logger)
         logger.info(f"Schema update completed successfully to version {latest_schema_version}.")
     except Exception as e:
         logger.error(f"Schema update failed: {e}")
