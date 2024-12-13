59825a6e93034809ff53021e32c73c799e9b934d
Umar Khan <ufkhan@emory.edu>
2024-12-13 18:23:42 +0800

    Update test_long_query.py

--- automations/test_long_query.py
+++ automations/test_long_query.py
@@ -1,6 +1,8 @@
 import os
 import psycopg2
 import logging
+import threading
+import time
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,9 +18,25 @@ DB_PARAMS = {
     'connect_timeout': 600,  # 10 minutes
 }
 
+def keepalive(conn):
+    """Send a keepalive query every 60 seconds"""
+    while True:
+        try:
+            with conn.cursor() as cur:
+                cur.execute('SELECT 1;')
+                logger.info('Keepalive query executed')
+            time.sleep(60)  # Wait 60 seconds before next keepalive
+        except:
+            break
+
 def long_running_query():
     try:
         conn = psycopg2.connect(**DB_PARAMS)
+        
+        # Start keepalive thread
+        keepalive_thread = threading.Thread(target=keepalive, args=(conn,), daemon=True)
+        keepalive_thread.start()
+        
         with conn.cursor() as cursor:
             logger.info('Starting long-running query (SELECT pg_sleep(600);)')
             cursor.execute('SELECT pg_sleep(600);')
